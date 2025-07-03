79392758f52471a8bf914448266e109cef288fc1
jipstavenuiter <jipstavenuiter@gmail.com>
2025-07-03 13:42:27 -0400

    chore/add-react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import React from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = React.useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </HydrationBoundary>
+    </QueryClientProvider>
+  );
 }
